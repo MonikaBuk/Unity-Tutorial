0439628d62ccb0f74cb19017f6346a032e6cf5e3
Monika B <monika2-bukovics@live.uwe.ac.uk>
2023-02-20 17:01:22 +0000

    camera updated

--- Assets/Scripts/Cameras/FollowCamera.cs
+++ Assets/Scripts/Cameras/FollowCamera.cs
@@ -6,6 +6,9 @@ public class FollowCamera : MonoBehaviour
 {
     public GameObject target;
     Vector3 offset;
+    private float mouseX;
+    private float mouseY;
+    private float mouseZ;
 
 
 
@@ -21,6 +24,18 @@ public class FollowCamera : MonoBehaviour
         Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0); 
         transform.position = target.transform.position + (rotation * offset);
         transform.LookAt(target.transform);
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+        mouseZ = Input.GetAxis("Mouse ScrollWheel");
+
+        if (Input.GetMouseButton(1))
+        {
+            offset = Quaternion.Euler(0, mouseX, 0) * offset;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            offset = Quaternion.Euler(mouseY, 0, 0) * offset;
+        }
     }
 
    
